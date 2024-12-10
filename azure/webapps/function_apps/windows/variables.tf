b4dc725209cc6ecd3b0df0b98afeb11006582a5c
ava-denzilbrown <157192613+ava-denzilbrown@users.noreply.github.com>
2024-12-10 10:30:38 -0500

    Create variables.tf

---
+++ azure/webapps/function_apps/windows/variables.tf
@@ -0,0 +1,35 @@
+########################################################
+#
+#  Function App Variables
+#
+########################################################
+
+variable "name" {
+  description = "tbd"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "tbd"
+  type        = string
+}
+
+variable "location" {
+  description = "tbd"
+  type        = string
+}
+
+variable "service_plan_id" {
+  description = "tbd"
+  type        = string
+}
+
+variable "app_settings" {
+  description = "tbd"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "tbd"
+  type        = string
+}
